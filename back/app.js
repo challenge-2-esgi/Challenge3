63ca87e18d9a6867c5334923bf7d6575ffb4c6d9
izemaghilas <saidtala23@gmail.com>
2023-10-11 15:35:12 +0200

    (back) generic crud router

---
+++ back/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const CRUDRouter = require('./routes/item-router')
+const ValidationError = require('./errors/ValidationError')
+
+// TODO: define cors options
+// const corsOptions = {}
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// TODO: add crud routes
+// ex: app.use('/users', CRUDRouter({ model: User }))
+
+// errors middleware
+app.use(function (err, req, res, next) {
+    if (err instanceof ValidationError) {
+        res.status(422).json(err.errors)
+    } else if (err instanceof SyntaxError) {
+        res.sendStatus(400)
+    } else {
+        console.log('[Error] : '+ err)
+        res.status(500).send(err.message)
+    }
+})
+
+module.exports = app
