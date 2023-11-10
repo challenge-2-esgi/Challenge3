9c5fe9968804e5af3ae938b3172cfeb8ab0e0f74
izemaghilas <saidtala23@gmail.com>
2023-11-10 23:02:50 +0100

    (front) add svgr

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -48,6 +48,26 @@ const config = {
             ...config.resolve.alias,
             '@': path.resolve(__dirname, '../src'),
         }
+
+        // https://react-svgr.com/docs/next/
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg')
+        )
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                resourceQuery: { not: /url/ }, // exclude if *.svg?url
+                use: ['@svgr/webpack'],
+            }
+        )
+        fileLoaderRule.exclude = /\.svg$/i
+
         return config
     },
 }
