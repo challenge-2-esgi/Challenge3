bd4f60c68853b2a4b4a493e98760ec2cbb3b1854
izemaghilas <saidtala23@gmail.com>
2023-11-18 00:55:05 +0100

    (mobile) theme colors

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/screens/login_screen.dart';
+import 'package:mobile/theme/app_theme.dart';
 
 void main() {
   runApp(const App());
@@ -10,9 +11,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Challenge',
-      home: LoginScreen(),
+      theme: AppTheme.themeData,
+      home: const LoginScreen(),
     );
   }
 }
