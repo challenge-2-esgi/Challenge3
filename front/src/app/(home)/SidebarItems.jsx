58f8d7bc6687e5707a0e3f4bb504879a20ee79b9
izemaghilas <saidtala23@gmail.com>
2023-11-19 00:04:58 +0100

    (front) sidebar items according to user role

--- front/src/app/(home)/SidebarItems.jsx
+++ front/src/app/(home)/SidebarItems.jsx
@@ -5,17 +5,41 @@ import { useTranslation } from 'react-i18next'
 import Sidebar from '@/components/Sidebar'
 import '@/i18n'
 import route from '../../constants/route'
+import useStore from '@/store'
+import { role } from '@/constants'
 
 export default () => {
     const { t } = useTranslation()
+    const user = useStore((state) => state.loggedinUser)
 
-    const menuItems = [
-        {
-            title: t('sidebar.item.dashboard'),
-            path: route.DASHBOARD,
-            icon: null,
-        },
-    ]
+    if (user == null) {
+        return null
+    }
+
+    const menuItems = buildMenuItem(user.role, t)
 
     return <Sidebar items={menuItems} />
+}
+
+function buildMenuItem(userRole, t) {
+    if (userRole === role.admin) {
+        return [
+            {
+                title: t('sidebar.item.dashboard'),
+                path: route.DASHBOARD,
+                icon: null,
+            },
+            {
+                title: t('sidebar.item.users'),
+                path: route.USERS,
+                icon: null,
+            },
+        ]
+    }
+
+    if (userRole === role.support) {
+        return []
+    }
+
+    return []
 }
