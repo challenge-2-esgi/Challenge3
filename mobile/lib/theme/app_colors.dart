699e4ade77a2037cd278606fc0677f26f4ebc38e
izemaghilas <saidtala23@gmail.com>
2023-11-18 01:00:17 +0100

    (mobile) theme colors

---
+++ mobile/lib/theme/app_colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff1c2434);
+  static const black2 = Color(0xff010101);
+  static const body = Color(0xff64748b);
+  static const bodyDark = Color(0xffaeb7c0);
+  static const bodyDark1 = Color(0xffdee4ee);
+  static const bodyDark2 = Color(0xff8a99af);
+  static const primary = Color(0xff3c50e0);
+  static const secondary = Color(0xff80caee);
+  static const stroke = Color(0xffe2e8f0);
+  static const gray = Color(0xffeff4fb);
+  static const grayDark = Color(0xff333a48);
+  static const gray2 = Color(0xfff7f9fc);
+  static const gray3 = Color(0xfffafafa);
+  static const whiten = Color(0xfff1f5f9);
+  static const whiter = Color(0xfff5f7fd);
+  static const boxDark = Color(0xff24303f);
+  static const boxDark2 = Color(0xff1a222c);
+  static const strokeDark = Color(0xff2e3a47);
+  static const formStrokeDark = Color(0xff3d4d60);
+  static const formInput = Color(0xff1d2a39);
+  static const meta1 = Color(0xffdc3545);
+  static const meta2 = Color(0xffeff2f7);
+  static const meta3 = Color(0xff10b981);
+  static const meta4 = Color(0xff313d4a);
+  static const meta5 = Color(0xff259ae6);
+  static const meta6 = Color(0xffffba00);
+  static const meta7 = Color(0xffff6766);
+  static const meta8 = Color(0xfff0950c);
+  static const meta9 = Color(0xffe5e7eb);
+  static const success = Color(0xff219653);
+  static const danger = Color(0xffd34053);
+  static const warning = Color(0xffffa70b);
+}
