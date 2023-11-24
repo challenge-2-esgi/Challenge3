fb16b50e1d94e1f246651fe648c9258f3278fe95
izemaghilas <saidtala23@gmail.com>
2023-11-24 15:37:25 +0100

    (back) use middlewars on crud operations

--- back/routes/item-router.js
+++ back/routes/item-router.js
@@ -5,11 +5,17 @@ const ValidationError = require('../errors/ValidationError')
 function ItemRouter({
     model: Model,
     allowedMethods = ['findAll', 'create', 'findOne', 'update', 'delete'],
+    // middlewares order is important
+    collectionMiddlewares = [],
+    itemCreateMiddlewares = [],
+    itemReadMiddlewares = [],
+    itemUpdateMiddlewares = [],
+    itemDeleteGuards = [],
 }) {
     const router = new Router()
 
     if (allowedMethods.includes('findAll')) {
-        router.get('/', async function (req, res) {
+        router.get('/', ...collectionMiddlewares, async function (req, res) {
             const items = await Model.findAll({
                 where: req.query,
             })
@@ -18,37 +24,45 @@ function ItemRouter({
     }
 
     if (allowedMethods.includes('create')) {
-        router.post('/', async function (req, res, next) {
-            try {
-                const id = uuidv7()
-                const item = await Model.create({ id, ...req.body })
-                res.status(201).json(item)
-            } catch (error) {
-                if (error.name === 'SequelizeValidationError') {
-                    error = ValidationError.fromSequelize(error)
+        router.post(
+            '/',
+            ...itemCreateMiddlewares,
+            async function (req, res, next) {
+                try {
+                    const id = uuidv7()
+                    const item = await Model.create({ id, ...req.body })
+                    res.status(201).json(item)
+                } catch (error) {
+                    if (error.name === 'SequelizeValidationError') {
+                        error = ValidationError.fromSequelize(error)
+                    }
+                    next(error)
                 }
-                next(error)
             }
-        })
+        )
     }
 
     if (allowedMethods.includes('findOne')) {
-        router.get('/:id', async function (req, res, next) {
-            try {
-                const item = await Model.findOne({
-                    where: {
-                        id: req.params.id,
-                    },
-                })
-                if (item) {
-                    res.json(item)
-                } else {
-                    res.sendStatus(404)
+        router.get(
+            '/:id',
+            ...itemReadMiddlewares,
+            async function (req, res, next) {
+                try {
+                    const item = await Model.findOne({
+                        where: {
+                            id: req.params.id,
+                        },
+                    })
+                    if (item) {
+                        res.json(item)
+                    } else {
+                        res.sendStatus(404)
+                    }
+                } catch (error) {
+                    next(error)
                 }
-            } catch (error) {
-                next(error)
             }
-        })
+        )
     }
 
     router.put('/:id', async function (req, res, next) {
@@ -66,41 +80,49 @@ function ItemRouter({
     })
 
     if (allowedMethods.includes('update')) {
-        router.patch('/:id', async function (req, res, next) {
-            try {
-                const id = req.params.id
-                const [_, items] = await Model.update(req.body, {
-                    where: { id },
-                    returning: true,
-                    individualHooks: true,
-                })
-                if (!items.length) {
-                    res.sendStatus(404)
-                } else {
-                    res.json(items[0])
+        router.patch(
+            '/:id',
+            ...itemUpdateMiddlewares,
+            async function (req, res, next) {
+                try {
+                    const id = req.params.id
+                    const [_, items] = await Model.update(req.body, {
+                        where: { id },
+                        returning: true,
+                        individualHooks: true,
+                    })
+                    if (!items.length) {
+                        res.sendStatus(404)
+                    } else {
+                        res.json(items[0])
+                    }
+                } catch (error) {
+                    if (error.name === 'SequelizeValidationError') {
+                        error = ValidationError.fromSequelize(error)
+                    }
+                    next(error)
                 }
-            } catch (error) {
-                if (error.name === 'SequelizeValidationError') {
-                    error = ValidationError.fromSequelize(error)
-                }
-                next(error)
             }
-        })
+        )
     }
 
     if (allowedMethods.includes('delete')) {
-        router.delete('/:id', async function (req, res, next) {
-            try {
-                const nbDeleted = await Model.destroy({
-                    where: {
-                        id: req.params.id,
-                    },
-                })
-                res.sendStatus(nbDeleted ? 204 : 404)
-            } catch (error) {
-                next(error)
+        router.delete(
+            '/:id',
+            ...itemDeleteGuards,
+            async function (req, res, next) {
+                try {
+                    const nbDeleted = await Model.destroy({
+                        where: {
+                            id: req.params.id,
+                        },
+                    })
+                    res.sendStatus(nbDeleted ? 204 : 404)
+                } catch (error) {
+                    next(error)
+                }
             }
-        })
+        )
     }
 
     return router
