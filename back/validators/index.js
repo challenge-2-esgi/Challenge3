b3054c41d902961f4860ebb2408b0284a2aa9410
Kagistan EESHVARAN <eeshvarankagistan@gmail.com>
2023-11-25 11:57:07 +0100

    (backend) - order router validator and guard

--- back/validators/index.js
+++ back/validators/index.js
@@ -1,9 +1,13 @@
 const login = require('./login.validator')
 const createUser = require('./create.user.validator')
 const updateUser = require('./update.user.validator')
+const createOrder = require('./create.order.validator')
+const updateOrder = require('./update.order.order')
 
 module.exports = {
     login,
     createUser,
     updateUser,
+    createOrder,
+    updateOrder,
 }
