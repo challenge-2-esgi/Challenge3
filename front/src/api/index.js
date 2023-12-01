83eea0b2d9b46f0a154d358437ee20805c75f646
jeremdh62 <jdhaille@myges.fr>
2023-12-01 15:01:14 +0100

    Add Rating service and update translations

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,5 +1,6 @@
 import Auth from './services/Auth'
 import User from './services/User'
 import Order from './services/Order'
+import Rating from './services/Rating'
 
-export { Auth, User, Order }
+export { Auth, User, Order, Rating }
