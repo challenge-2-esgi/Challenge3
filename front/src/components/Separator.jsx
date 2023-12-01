da6f4dce717b6582acd6a602ff728c9c3f0cb406
Kagistan EESHVARAN <eeshvarankagistan@gmail.com>
2023-12-01 14:41:07 +0100

    (frontend) - separtor component

---
+++ front/src/components/Separator.jsx
@@ -0,0 +1,9 @@
+const Separator = () => {
+    return (
+        <div>
+            <div className="mb-12 mt-12 flex h-[0.5px] w-full bg-[gray]"></div>
+        </div>
+    )
+}
+
+export default Separator
