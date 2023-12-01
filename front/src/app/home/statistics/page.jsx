8d2a8a80b56f6c688fd325e2da84f8f4fca48718
Kagistan EESHVARAN <eeshvarankagistan@gmail.com>
2023-12-01 12:36:48 +0100

    (frontend) - statitics rechart line chart dashboard

---
+++ front/src/app/home/statistics/page.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Order } from '@/api'
+import { useRouter } from 'next/navigation'
+import { useTranslation } from 'react-i18next'
+import { t_NAMESPACES } from '@/i18n'
+import DeliveriesPerDay from './DeliveriesPerDay'
+import StatisticCards from './StatisticCards'
+
+const Statistics = ({ params }) => {
+    const { t } = useTranslation()
+    const router = useRouter()
+    const { data, isLoading: fetchingOrder } = Order.useOrder('')
+
+    return (
+        <>
+            <div className="flex w-full flex-col">
+                <p className="mb-5 text-center text-xl font-bold">
+                    {t('dashboard.main_title')}
+                </p>
+                <StatisticCards />
+                <DeliveriesPerDay />
+            </div>
+        </>
+    )
+}
+
+export default Statistics
