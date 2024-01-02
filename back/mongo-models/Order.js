3d966d322dafbe60315a0fe0e1e7ed1410ab3b37
izemaghilas <saidtala23@gmail.com>
2024-01-02 15:39:11 +0100

    (back) add mongo schemas

---
+++ back/mongo-models/Order.js
@@ -0,0 +1,50 @@
+const { mongoose } = require('../db')
+
+const addressSchemaDef = {
+    streetNumber: String,
+    street: String,
+    zipCode: String,
+    city: String,
+    country: String,
+    latitude: Number,
+    longitude: Number,
+}
+const userSchemaDef = {
+    id: String,
+    firstname: String,
+    lastname: String,
+    email: String,
+}
+
+const schema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            alias: 'id',
+        },
+        sku: String,
+        isDelivered: Boolean,
+        pickupTime: Date,
+        deliverTime: Date,
+        validationCode: String,
+        distance: Number,
+        status: String,
+        receiverFirstname: String,
+        receiverLastname: String,
+        receiverEmail: String,
+        receiverPhone: String,
+        pickupAddress: addressSchemaDef,
+        deliveryAddress: addressSchemaDef,
+        user: userSchemaDef,
+        deliverer: userSchemaDef,
+    },
+    { collection: 'orders' }
+)
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+})
+
+const Order = mongoose.model('Order', schema)
+
+module.exports = Order
