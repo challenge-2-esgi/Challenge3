3c5937ffd3947f215ae4ab3836b98da489f13bdd
izemaghilas <saidtala23@gmail.com>
2024-01-08 15:38:50 +0000

    deploy back

--- routes/statistics.js
+++ routes/statistics.js
@@ -10,6 +10,27 @@ const MongoUser = require('../mongo-models/User')
 function StatisticsRouter() {
     const router = new Router()
 
+    // Endpoint to get the number of active deliverers
+    router.get(
+        '/active-deliverers-count',
+        AuthGuard,
+        RolesGuard([ROLE.admin]),
+        async (req, res) => {
+            try {
+                // Query MongoDB for the count of active deliverers
+                const activeDeliverersCount = await MongoUser.countDocuments({
+                    role: ROLE.deliverer,
+                    isActive: true,
+                })
+
+                res.json({ activeDeliverersCount })
+            } catch (error) {
+                console.error(error)
+                res.status(500).send('Internal Server Error')
+            }
+        }
+    )
+
     // Endpoint to get the number of deliveries made per day
     router.get(
         '/deliveries-per-day',
@@ -17,16 +38,38 @@ function StatisticsRouter() {
         RolesGuard([ROLE.admin]),
         async (req, res) => {
             try {
-                // Calculate the start of the current day
-                const currentDate = moment().startOf('day')
+                // Calculate the start of the last 7 days
+                const startOfLast7Days = moment()
+                    .subtract(7, 'days')
+                    .startOf('day')
 
-                // Query MongoDB for the count of delivered orders on the current day
-                const deliveriesCount = await MongoOrder.countDocuments({
-                    isDelivered: true,
-                    deliverTime: { $gte: currentDate.toDate() },
-                })
+                // Response array to store counts for each day
+                const response = []
 
-                res.json({ deliveriesCount })
+                // Loop through each day within the last 7 days
+                let currentDate = moment(startOfLast7Days)
+                while (currentDate.isSameOrBefore(moment())) {
+                    // Query MongoDB for the count of delivered orders on the current day
+                    const deliveriesCount = await MongoOrder.countDocuments({
+                        isDelivered: true,
+                        deliverTime: {
+                            $gte: currentDate.toDate(),
+                            $lt: currentDate.clone().endOf('day').toDate(),
+                        },
+                    })
+
+                    const currentDay = currentDate.format('DD-MM-YYYY')
+
+                    // Add counts to the response array for the current day
+                    response.push({
+                        date: currentDay,
+                        nbOfDeliveries: deliveriesCount,
+                    })
+
+                    currentDate.add(1, 'day')
+                }
+
+                res.json(response)
             } catch (error) {
                 console.error(error)
                 res.status(500).send('Internal Server Error')
@@ -73,13 +116,13 @@ function StatisticsRouter() {
                     .subtract(7, 'days')
                     .startOf('day')
 
-                // Response object to store counts for each day
-                const response = {}
+                // Response array to store counts for each day
+                const response = []
 
                 // Loop through each day within the last 7 days
                 let currentDate = moment(startOfLast7Days)
                 while (currentDate.isSameOrBefore(moment())) {
-                    const currentDay = currentDate.format('YYYY-MM-DD')
+                    const currentDay = currentDate.format('DD-MM-YYYY')
 
                     // Query MongoDB for the counts of new users and new deliverers created on the current day
                     const newUsers = await MongoUser.countDocuments({
@@ -98,16 +141,54 @@ function StatisticsRouter() {
                         role: 'DELIVERER', // 'DELIVERER' is the role for deliverers
                     })
 
-                    // Add counts to the response object for the current day
-                    response[currentDay] = {
+                    // Add counts to the response array for the current day
+                    response.push({
+                        date: currentDay,
                         nbNewUsers: newUsers,
                         nbNewDeliverers: newDeliverers,
-                    }
+                    })
 
                     currentDate.add(1, 'day')
                 }
 
                 res.json(response)
+            } catch (error) {
+                console.error(error)
+                res.status(500).send('Internal Server Error')
+            }
+        }
+    )
+
+    // Endpoint to get the average delivery time for all time
+    router.get(
+        '/average-delivery-time',
+        AuthGuard,
+        RolesGuard([ROLE.admin]),
+        async (req, res) => {
+            try {
+                // Query MongoDB for all delivered orders
+                const orders = await MongoOrder.find({ isDelivered: true })
+
+                // Calculate the total delivery time and count of orders
+                let totalDeliveryTime = 0
+                let ordersCount = 0
+
+                orders.forEach((order) => {
+                    if (order.deliverTime && order.pickupTime) {
+                        // Calculate the delivery time in minutes
+                        const deliveryTimeInMinutes = moment(
+                            order.deliverTime
+                        ).diff(moment(order.pickupTime), 'minutes')
+                        totalDeliveryTime += deliveryTimeInMinutes
+                        ordersCount++
+                    }
+                })
+
+                // Calculate the average delivery time for all time
+                const averageDeliveryTime =
+                    ordersCount > 0 ? totalDeliveryTime / ordersCount : 0
+
+                res.json({ averageDeliveryTime })
             } catch (error) {
                 console.error(error)
                 res.status(500).send('Internal Server Error')
